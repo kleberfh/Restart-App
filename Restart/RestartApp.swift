d8056a36dd0dccaaf198945dcf8df7e0fc6709d4
Kleber Fernando <canedokleberfilho@gmail.com>
2022-02-27 01:00:10 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Kleber Fernando on 27/02/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
